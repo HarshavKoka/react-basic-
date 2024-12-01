72b10f25a7294e7e87e193991bd6e2bdac4cae0a
HarshavKoka <kokaharsha2203@gmail.com>
2024-12-01 23:38:45 +0530

    complete my react project

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,33 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Die from './Die'
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+
 
 const App = () => {
+  const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+
+    }
+  }, [dice])
+
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice() {
-
     const newDice = []
-
     for (let i = 0; i < 10; i++) {
-
-      newDice.push(Math.ceil(Math.random() * 6))
+      newDice.push(generateNewDie()
+      )
     }
     return newDice
   }
 
-  console.log(allNewDice())
+  function rollDice() {
+    if (!tenzies) {
+      setDice(oldDice =>
+        oldDice.map(die =>
+          die.isHeld ? die : generateNewDie()
+
+        )
+      )
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
+    }))
+  }
+
+  const diceElements = dice.map(die => <Die
+    value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
   return (
     <main>
+      {tenzies && <Confetti />}
+      <h1 className='title'>Tenzies</h1><p>
+        Roll untill all dice are the same.Click each die to freeze it as its current value between rolls.
+      </p>
       <div className='dice-container'>
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="4" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
+        {diceElements}
       </div>
+      <button className='roll-dice' onClick={rollDice}>{tenzies ? "New game" : "Roll"}</button>
+
     </main>
   )
 }
